380dea4551f77efadb5ee9b518803104272a8192
Saravanakumar1802 <saravana1802kumar@gmail.com>
2023-05-28 12:34:32 +0530

    commit

---
+++ middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const protect = async (req, res, next) => {
+  try {
+
+    const token = req.cookies.access_token;
+
+    if (!token) {
+      res.status(401);
+      return next(new Error("Not authorized, no token"));
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = { _id: decoded._id };
+    next();
+  } catch (error) {
+    res.status(401);
+    return next(new Error("Not authorized"));
+  }
+};
+
+module.exports = protect;
