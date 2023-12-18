5c5c13b47d4f669f24c36331b50478c052dab763
Saravanakumar1802 <saravana1802kumar@gmail.com>
2023-12-18 10:59:15 +0530

    error fix

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    // console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
   }
